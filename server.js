4bf5a86ce592cfd04b10a472d6a872ee23121cbd
MikeEddards <mikeeddards@gmail.com>
2021-03-17 07:30:34 -0600

    change

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.json())
-app.use( express.static( './index.html' ) );
-app.listen(4000, () => console.log(`All ears on port: 4000`))
